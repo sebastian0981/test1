d46549ad8628fdcb9fd701517ddb58ce10d2f400
GNSB <gr.sobrevia@alumnos.duoc.cl>
2021-01-27 14:51:41 -0300

    loggin 2.0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-function x() { console.log("seba puto") }
+function login() {
+    if (document.form.user.value == "GNSB" && document.form.pass.value == "GNSB1110") {
+        location.href = "logged.html";
+        console.log("LOGGED")
+    } else {
+        alert("USUARIO O CONTRASEÑA INCORRECTOS.")
+    }
+
+
+}
