fcf9eefa2123d1a67837c6f7499c3d7bcd819965
GNSB <gr.sobrevia@alumnos.duoc.cl>
2021-01-28 13:55:07 -0300

    cambios al botón submit -> pasa a button por problemas con href

--- Greg/login.js
+++ Greg/login.js
@@ -1,6 +1,6 @@
 function login() {
     if (document.form.user.value == "GNSB" && document.form.pass.value == "GNSB1110") {
-        location.href = "logged.html";
+        location.href = "http://google.cl";
     } else {
         alert("USUARIO O CONTRASEÑA INCORRECTOS.")
     }
