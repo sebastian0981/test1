2715cebbd7e90fc6708cfa074a4111103fd68312
GNSB <gr.sobrevia@alumnos.duoc.cl>
2021-01-29 14:07:41 -0300

    cambio a algunas funciones, parte de la redirección y un css loading effect.

--- Greg/login.js
+++ Greg/login.js
@@ -1,8 +1,15 @@
 function login() {
     if (document.form.user.value == "GNSB" && document.form.pass.value == "GNSB1110") {
-        location.href = "http://google.cl";
+        //location.href = "logged.html";
+        delay();
+        console.log("hola, han pasado 5 segundos.")
+
     } else {
         alert("USUARIO O CONTRASEÑA INCORRECTOS.")
     }
 
+}
+
+function delay() {
+    setTimeout(5000);
 }
